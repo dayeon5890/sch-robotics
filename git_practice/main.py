549f0d3d168e6ea9d1c40f8ada9e3fb1d930543b
student.dayeon <dayeon5890@naver.com>
2020-11-16 20:56:57 +0900

    change main to use list ops

--- git_practice/main.py
+++ git_practice/main.py
@@ -1,11 +1,10 @@
-<<<<<<< HEAD
-if __name__ == "__main__":
-    print("hello python, hi ros.")
-    print("Python is the best language for ros")
-=======
-from add_lists import add_lists
+# main.py
+import list_ops as lo
 
 if __name__ == "__main__":
-    result = add_lists(['wel', 't', 'r'], ['come', 'o', 'os'])
-    print(result)
->>>>>>> 95f1e7112f5cba4075b1566cf26ac141a608faec
+    foo = [1,2,3]
+    bar = [4,5,6]
+    print("foo+bar=", lo.add(foo, bar))
+    print("foo-bar=", lo.subtract(foo, bar))
+    print("foo*bar=", lo.multiply(foo, bar))
+    print("foo/bar=", lo.divide(foo, bar))
